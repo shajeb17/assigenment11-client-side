62c6b4409894a2840df37895c9fe441bb57e355b
unknown <shajeb17.11@gmail.com>
2025-12-14 09:44:34 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Component/Navbar/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div>
+                <Navbar></Navbar>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
